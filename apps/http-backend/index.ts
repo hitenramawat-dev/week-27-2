bea86b28c0ddcd1a2d27e7dcff082f09ea607c37
Hiten Ramawat <hitenramawat1010@gmail.com>
2025-02-22 12:13:01 +0530

    first-commit

---
+++ apps/http-backend/index.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+
+
+app.post('/create/todo',async(req,res) => {
+    
+})
+
+
+
+app.listen(3000,() => {
+    console.log('we Running');
+})
---
+++ apps/ws/index.ts
@@ -0,0 +1,20 @@
+import { client } from "@repo/db/client";
+import { WebSocketServer } from "ws";
+
+const server = new WebSocketServer({port:8080})
+
+server.on('connection',async(ws:WebSocket) => {
+    console.log("Client connected");
+
+    await client.user.create({
+        data:{
+            username:Math.random().toString(),
+            password:Math.random().toString()
+        }
+    })
+
+    ws.on('message',(data) => {
+        ws.send(data)
+    })
+
+})
